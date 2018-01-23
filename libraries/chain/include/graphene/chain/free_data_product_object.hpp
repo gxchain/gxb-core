8a24654138b28f9a4d744cf9c5ec8779ae4f4f4c
albert <zhuliting@gxb.io>
2018-01-23 19:50:20 +0800

    Use fc::string instead of std::string

--- libraries/chain/include/graphene/chain/free_data_product_object.hpp
+++ libraries/chain/include/graphene/chain/free_data_product_object.hpp
@@ -40,8 +40,8 @@ namespace graphene {
         public:
             static const uint8_t space_id = protocol_ids;
             static const uint8_t type_id = free_data_product_object_type;
-            string                                  product_name;
-            string                                  brief_desc;
+            fc::string                              product_name;
+            fc::string                              brief_desc;
             // provider of data product
             account_id_type                         datasource;
             // data market categoiry id
@@ -52,11 +52,11 @@ namespace graphene {
             // 0 --> init, 1 --> online, 2 --> offline
             uint8_t                                 status;
             // icon of data product
-            string                                  icon;
+            fc::string                              icon;
             // schema of input/output in data_transaction
             vector<schema_context_object>           schema_contexts;
             // product parent_id, can be empty
-            string                                  parent_id;
+            fc::string                              parent_id;
             time_point_sec                          create_date_time;
             time_point_sec                          recommend_expiration_date_time;
             // total data_transaction count
