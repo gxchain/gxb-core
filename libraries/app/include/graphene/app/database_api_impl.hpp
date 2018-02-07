0251e4627859382966164c2174f34143108819f1
tutu <137112104@qq.com>
2018-02-07 16:45:30 +0800

    add get_block_by_id

--- libraries/app/include/graphene/app/database_api_impl.hpp
+++ libraries/app/include/graphene/app/database_api_impl.hpp
@@ -93,6 +93,7 @@ class database_api_impl : public std::enable_shared_from_this<database_api_impl>
       // Blocks and transactions
       optional<block_header> get_block_header(uint32_t block_num)const;
       optional<signed_block_with_info> get_block(uint32_t block_num)const;
+      optional<signed_block_with_info> get_block_by_id(block_id_type block_id)const;
       processed_transaction get_transaction( uint32_t block_num, uint32_t trx_in_block )const;
 
       // Globals
