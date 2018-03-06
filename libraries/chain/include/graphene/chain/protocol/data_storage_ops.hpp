de81830f1658ebc3f4520bdac59db37d18ab3ac1
zhuliting <zhuliting@foxmail.com>
2018-03-06 15:38:49 +0800

    updated data_storage_operation

--- libraries/chain/include/graphene/chain/protocol/data_storage_ops.hpp
+++ libraries/chain/include/graphene/chain/protocol/data_storage_ops.hpp
@@ -19,38 +19,48 @@
 #pragma once
 #include <graphene/chain/protocol/base.hpp>
 #include <graphene/chain/protocol/ext.hpp>
-#include <graphene/chain/protocol/data_storage_params.hpp>
 
 namespace graphene { namespace chain {
-
-struct data_storage_operation : public base_operation {
-    struct fee_parameters_type {
-        uint64_t fee = 0;
+    struct  data_storage_params
+    {
+        asset                   fee;
+        fc::string              data_md5;
+        fc::time_point_sec      expiration;
     };
 
-    account_id_type                 proxy_account;
-    account_id_type                 account;
-    fc::string                      data_hash;
-    asset                           fee;
-    data_storage_params             params;
-    signature_type                  signature;
-    extensions_type                 extensions;
+    struct data_storage_operation : public base_operation {
+        struct fee_parameters_type {
+            uint64_t fee = 0;
+        };
 
-    account_id_type fee_payer() const { return proxy_account; }
+        account_id_type                 proxy_account;
+        account_id_type                 account;
+        fc::string                      data_hash;
+        asset                           fee;
+        data_storage_params             params;
+        signature_type                  signature;
+        extensions_type                 extensions;
 
-    void validate() const
-    {
-        FC_ASSERT(data_hash.size() > 0);
-    }
+        account_id_type fee_payer() const { return proxy_account; }
 
-    share_type calculate_fee(const fee_parameters_type &k) const
-    {
-        return k.fee;
-    }
-};
+        void validate() const
+        {
+            FC_ASSERT(data_hash.size() > 0);
+        }
+
+        share_type calculate_fee(const fee_parameters_type &k) const
+        {
+            return k.fee;
+        }
+    };
 
 
 } } // graphene::chain
+
+FC_REFLECT(graphene::chain::data_storage_params,
+            (fee)
+            (data_md5)
+            (expiration))
 
 FC_REFLECT(graphene::chain::data_storage_operation::fee_parameters_type, (fee))
 
