11738c75f664e0b5e19e5b7a801f757845f20bf6
zhuliting <zhuliting@foxmail.com>
2018-04-09 17:52:05 +0800

    update test case

--- tests/tests/operation_tests2.cpp
+++ tests/tests/operation_tests2.cpp
@@ -92,15 +92,8 @@ BOOST_AUTO_TEST_CASE(proxy_transfer_test)
    ACTOR(bob);
    ACTOR(dan);
 
-   // reserve core
-   fund_fee_pool(account_id_type()(db), get_asset(GRAPHENE_SYMBOL), GRAPHENE_MAX_SHARE_SUPPLY / 2);
-   generate_block();
-
-   // issue GXS
-   asset_id_type gxs_id = create_user_issued_asset(GRAPHENE_SYMBOL_GXS, alice_id(db), 0).id;
-   issue_uia(alice_id, asset(50000, gxs_id));
-   // transfer(account_id_type(), alice_id, asset(1000000));
-   transfer(alice_id, bob_id, asset(10000, gxs_id));
+   transfer(account_id_type(), alice_id, asset(10000));
+   transfer(account_id_type(), bob_id, asset(10000));
    generate_block();
 
    // construct trx
@@ -108,15 +101,15 @@ BOOST_AUTO_TEST_CASE(proxy_transfer_test)
    param.from = alice_id;
    param.to = dan_id;
    param.proxy_account = bob_id;
-   param.percentage = 10000; // 10%
-   param.amount = asset(5000, gxs_id);
+   param.percentage = 1000; // 10%
+   param.amount = asset(5000);
    param.memo = fc::json::to_string(alice_private_key.get_public_key());
    param.expiration = db.head_block_time() + fc::hours(1);
    param.signatures.push_back(sign_proxy_transfer_param(alice_private_key, param));
 
    proxy_transfer_operation op;
    op.proxy_memo = fc::json::to_string(bob_private_key.get_public_key());
-   op.fee = asset(2000, gxs_id);
+   op.fee = asset(2000);
    op.request_params = param;
 
    trx.clear();
@@ -128,8 +121,8 @@ BOOST_AUTO_TEST_CASE(proxy_transfer_test)
 
    // check balance
    BOOST_TEST_MESSAGE("check account balances");
-   const auto &core = gxs_id(db);
-   BOOST_REQUIRE_EQUAL(get_balance(alice_id(db), core), 35000); // 40000 - 5000
+   const auto &core = asset_id_type()(db);
+   BOOST_REQUIRE_EQUAL(get_balance(alice_id(db), core), 5000); // 10000 - 5000
    BOOST_REQUIRE_EQUAL(get_balance(dan_id(db), core), 4500); // 5000 - (5000 * 10%)
    BOOST_REQUIRE_EQUAL(get_balance(bob_id(db), core), 8500); // 10000 - 2000 + (5000 * 10%)
 } FC_LOG_AND_RETHROW() }
