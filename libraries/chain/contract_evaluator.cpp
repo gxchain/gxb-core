81e141af9925079a7d2253a059a23cb73f49a008
zhuliting <zhuliting@foxmail.com>
2018-05-25 16:49:32 +0800

    update contract call apply

--- libraries/chain/contract_evaluator.cpp
+++ libraries/chain/contract_evaluator.cpp
@@ -71,16 +71,13 @@ void_result contract_call_evaluator::do_apply(const contract_call_operation &op)
 
     auto wasm_bytes = bytes(wasm.begin(), wasm.end());
     dlog("wast code ${c}, code_id ${i}", ("c", wast_code)("i", code_id));
+
     try {
         action a{1, 1, {}};
         apply_context ap{a};
-        // wasm_interface(graphene::chain::wasm_interface::vm_type::binaryen).apply(code_id, wasm_bytes, ap);
-        graphene::chain::wasm_interface::vm_type runtime = graphene::chain::wasm_interface::vm_type::binaryen;
-        dlog("runtime ${r}", ("r", runtime));
-        wasm_interface wi(runtime);
-        wi.apply(code_id, wasm_bytes, ap);
+        wasm_interface(graphene::chain::wasm_interface::vm_type::binaryen).apply(code_id, wasm_bytes, ap);
         dlog("wasm exec success");
-    } catch ( const wasm_exit& ){
+    } catch (...) {
         dlog("wasm exec failed");
     }
 
