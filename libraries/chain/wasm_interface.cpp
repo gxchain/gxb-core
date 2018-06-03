01dfab730df38755535899635930e741e2878273
zhuliting <zhuliting@foxmail.com>
2018-06-03 19:01:59 +0800

    enable print

--- libraries/chain/wasm_interface.cpp
+++ libraries/chain/wasm_interface.cpp
@@ -268,7 +268,7 @@ class console_api : public context_aware_api {
    public:
       console_api( apply_context& ctx )
       : context_aware_api(ctx,true)
-      , ignore(true) {}
+      , ignore(false) {}
 
       // Kept as intrinsic rather than implementing on WASM side (using prints_l and strlen) because strlen is faster on native side.
       void prints(null_terminated_ptr str) {
