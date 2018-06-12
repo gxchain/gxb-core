4149fbf0d3440c4b33545f53964f9b1241f7cdf1
zhuliting <zhuliting@foxmail.com>
2018-06-12 17:09:00 +0800

    use distcc

--- compile.sh
+++ compile.sh
@@ -6,6 +6,7 @@ export WASM_LLVM_CONFIG=~/wasm-compiler/llvm/lib/cmake/llv
 export WASM_ROOT=~/wasm-compiler/llvm
 
 export DISTCC_HOSTS="192.168.100.158 192.168.100.169"
+export set CCACHE_PREFIX=distcc
 export CC='ccache gcc'
 export CXX='ccache g++'
 
@@ -17,4 +18,4 @@ fi
 
 rm -rf build &&  mkdir -p build &&  cd build
 cmake -DLLVM_DIR=$LLVM_DIR -DWASM_ROOT=$WASM_ROOT -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl -DOPENSSL_INCLUDE_DIR=/usr/local/opt/openssl/include -DOPENSSL_LIBRARIES=/usr/local/opt/openssl/lib -DCMAKE_BUILD_TYPE=Release ..
-make -j4
+make -j8
