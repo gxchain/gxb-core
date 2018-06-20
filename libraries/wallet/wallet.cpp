0cfee7f41d1f43adcd4764e1820d3cb484c90d20
liukunyu <boss.liuky@gmail.com>
2018-06-20 15:03:38 +0800

    update

--- libraries/wallet/wallet.cpp
+++ libraries/wallet/wallet.cpp
@@ -936,9 +936,6 @@
                FC_ASSERT(is_valid_name(contract));//todo fixme contract name must [a-z 0-5]
                
                account_object contract_account = this->get_account(contract);
-               if(contract_account == nullptr) {
-                   GRAPHENE_ASSERT(false, contract_not_found_exception, "No contract found for ${contract}", ("contract", contract));
-               }
     
                abi_def abi;
                bool table_exist = false;
