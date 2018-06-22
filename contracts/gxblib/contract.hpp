d14b3f8903730c003bc12294c664041d0706e444
zhuliting <zhuliting@foxmail.com>
2018-06-22 15:59:10 +0800

    add account balance for base contract

--- contracts/gxblib/contract.hpp
+++ contracts/gxblib/contract.hpp
@@ -2,17 +2,58 @@
 
 namespace graphene {
 
-class contract {
-   public:
-     contract(account_name n)
-         : _self(n)
-     {
-     }
+class contract
+{
+  public:
+    contract(account_name n)
+        : _self(n)
+    {
+    }
 
-     inline account_name get_self() const { return _self; }
+    inline account_name get_self() const { return _self; }
 
-   protected:
-      account_name _self;
+  protected:
+    account_name _self;
+
+  private:
+    struct account {
+        asset balance;
+
+        uint64_t primary_key() const { return balance.symbol.name(); }
+    };
+
+    typedef gxblib::multi_index<N(accounts), account> accounts;
+
+    void sub_balance(account_name owner, asset value)
+    {
+        accounts from_acnts(_self, owner);
+
+        const auto &from = from_acnts.get(value.symbol.name(), "no balance object found");
+        eosio_assert(from.balance.amount >= value.amount, "overdrawn balance");
+
+        if (from.balance.amount == value.amount) {
+            from_acnts.erase(from);
+        } else {
+            from_acnts.modify(from, owner, [&](auto &a) {
+                a.balance -= value;
+            });
+        }
+    }
+
+    void add_balance(account_name owner, asset value, account_name ram_payer)
+    {
+        accounts to_acnts(_self, owner);
+        auto to = to_acnts.find(value.symbol.name());
+        if (to == to_acnts.end()) {
+            to_acnts.emplace(ram_payer, [&](auto &a) {
+                a.balance = value;
+            });
+        } else {
+            to_acnts.modify(to, 0, [&](auto &a) {
+                a.balance += value;
+            });
+        }
+    }
 };
 
 } /// namespace graphene
