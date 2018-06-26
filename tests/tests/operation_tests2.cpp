1016cc846faf231c3dfa621d233f03d3104eab5c
zhuliting <zhuliting@foxmail.com>
2018-06-26 21:56:02 +0800

    unit test

--- tests/tests/operation_tests2.cpp
+++ tests/tests/operation_tests2.cpp
@@ -147,8 +147,7 @@ BOOST_AUTO_TEST_CASE(contract_test)
    auto wasm = graphene::chain::wast_to_wasm(contract_test_wast_code);
    deploy_op.code = bytes(wasm.begin(), wasm.end());
    deploy_op.code_version = fc::sha256::hash(deploy_op.code);
-   const char *abi = "abi";
-   deploy_op.abi = bytes(abi, abi+strlen(abi));
+   deploy_op.abi = bytes(contract_abi, contract_abi+strlen(contract_abi));
    deploy_op.fee = asset(2000);
    trx.operations.push_back(deploy_op);
    set_expiration(db, trx);
@@ -209,8 +208,7 @@ BOOST_AUTO_TEST_CASE(deposit_contract_test)
    auto wasm = graphene::chain::wast_to_wasm(contract_test_wast_code);
    deploy_op.code = bytes(wasm.begin(), wasm.end());
    deploy_op.code_version = fc::sha256::hash(deploy_op.code);
-   const char *abi = "abi";
-   deploy_op.abi = bytes(abi, abi+strlen(abi));
+   deploy_op.abi = bytes(contract_abi, contract_abi+strlen(contract_abi));
    deploy_op.fee = asset(2000);
    trx.operations.push_back(deploy_op);
    set_expiration(db, trx);
