7a45e517ca1ad7c8606a7d936d9097b4fc4e4de0
zhuliting <zhuliting@foxmail.com>
2018-07-10 22:20:20 +0800

    add fee reflect

--- libraries/chain/include/graphene/chain/protocol/contract_ops.hpp
+++ libraries/chain/include/graphene/chain/protocol/contract_ops.hpp
@@ -127,7 +127,7 @@ FC_REFLECT(graphene::chain::contract_deploy_operation,
             (abi)
             (extensions))
 
-FC_REFLECT(graphene::chain::contract_call_operation::fee_parameters_type, (fee))
+FC_REFLECT(graphene::chain::contract_call_operation::fee_parameters_type, (fee)(price_per_kbyte_ram)(price_per_ms_cpu))
 FC_REFLECT(graphene::chain::contract_call_operation, (fee)(account)(act)(extensions))
 
 FC_REFLECT(graphene::chain::contract_deposit_operation::fee_parameters_type, (fee))
