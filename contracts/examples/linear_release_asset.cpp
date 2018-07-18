63345a41c67018a30ea97a68dc08934993b8344b
liukunyu <boss.liuky@gmail.com>
2018-07-18 13:41:08 +0800

    update example of linear release asset

--- contracts/examples/linear_release_asset.cpp
+++ contracts/examples/linear_release_asset.cpp
@@ -1,60 +1,115 @@
-#include <gxblib/gxb.hpp>
+#include <gxblib/asset.h>
+#include <gxblib/asset.hpp>
+#include <gxblib/contract.hpp>
+#include <gxblib/dispatcher.hpp>
+#include <gxblib/global.h>
+#include <gxblib/multi_index.hpp>
+#include <gxblib/types.h>
+#include <vector>
 
 using namespace graphene;
 
-class skeleton
+//TODO 目前仅支持核心资产
+class skeleton : public contract
 {
-  public:
-    skeleton(account_name n)
-        : _self(n)
-        , accounts(_self, _self)
-        , lockrules(_self, _self)
-    {
-    }
-
+  private:
     //@abi table account i64
     struct account {
         account_name owner;
-        vector<asset> assets;
+        std::vector<asset> assets;
 
         uint64_t primary_key() const { return owner; }
 
         GXBLIB_SERIALIZE(account, (owner)(assets))
     };
 
+    //@abi table lockrule i64
+    struct lockrule {
+        uint64_t account;           //某个合约参与者的账号
+        int64_t lock_time_point;    //锁定开始时间
+        int64_t lock_duration;      //锁定多久开始释放
+        int64_t release_time_point; //释放开始时间
+        int64_t release_duration;   //释放多久全部释放完毕
+        uint64_t asset_id;          //资产id
+        int64_t asset_amount;       //总共锁定多少资产
+        int64_t released_amount;    //总共已经释放了多少资产
+
+        uint64_t primary_key() const { return account; }
+
+        GXBLIB_SERIALIZE(lockrule, (account)(lock_time_point)(lock_duration)(release_time_point)(release_duration)(asset_id)(asset_amount)(released_amount))
+    };
+
     typedef graphene::multi_index<N(account), account> account_index;
+    typedef graphene::multi_index<N(lockrule), lockrule> lockrule_index;
 
-    inline account_name get_self() const { return _self; }
-
-    /// @abi action
-    void deposit(account_name from, asset value)
+  public:
+    skeleton(account_name n)
+        : contract(n)
+        , accounts(_self, _self)
+        , lockrules(_self, _self)
     {
-        // from must be msg.sender
-        print("deposit trx origin: ", get_trx_origin(), "\n");
-        print("deposit trx from: ", from, "\n");
-        print("deposit asset_id: ", value.asset_id, ", amount:", value.amount, "\n");
-        gxb_assert(get_trx_origin() == from, "no deposit permission");
-        // check amount
-        // gxb_assert(get_trx_value() == value.amount);
-
-        transfer_asset(from, _self, value.asset_id, value.amount);
-        addbalance(from, value);
-        print("balance: ", getbalance(from, value.asset_id), "\n");
     }
 
     /// @abi action
-    void withdraw(account_name from, account_name to, asset value)
+    void lockasset(uint64_t from, account_name to, asset value)
     {
-        // from must be msg.sender
-        print("withdraw trx origin: ", get_trx_origin(), "\n");
-        print("withdraw trx from: ", from, "\n");
-        print("withdraw asset_id: ", value.asset_id, ", amount:", value.amount, "\n");
-        // gxb_assert(get_trx_origin() == from, "no withdraw permission");
-        // check amount
-        // gxb_assert(get_balance(from, value.symbol) >= value.amount);
+        auto lr = lockrules.find(from);
+        gxb_assert(lr == lockrules.end(), "have been locked, can only lock one time");
 
-        subbalance(from, value);
-        transfer_asset(_self, to, value.asset_id, value.amount);
+        lockrules.emplace(from, [&](auto &a) {
+            a.account = to;
+            a.lock_time_point = get_head_block_time();
+            a.lock_duration = 30;
+            a.release_time_point = a.lock_time_point + a.lock_duration;
+            a.release_duration = 3 * 100;
+            a.asset_id = value.asset_id;
+            a.asset_amount = value.amount;
+            a.released_amount = 0;
+        });
+
+        deposit_asset(from, _self, value.asset_id, value.amount);
+        addbalance(to, value);
+    }
+
+    /// @abi action
+    void tryrelease(uint64_t who)
+    {
+        auto lr = lockrules.find(who);
+        gxb_assert(lr != lockrules.end(), "have no locked asset, no lockrule");
+
+        uint64_t now = get_head_block_time();
+        if (now <= lr->release_time_point) {
+            print("within lock duration, can not release", "\n");
+            return;
+        }
+
+        auto who_it = accounts.find(who);
+        gxb_assert(who_it != accounts.end(), "have no locked asset, no asset");
+
+        int percentage = (now - lr->release_time_point) * 100 / lr->release_duration;
+        if (percentage > 100)
+            percentage = 100;
+        print("percentage=", percentage, "\n");
+
+        int64_t should_release_amount = (int64_t)(1.0f * lr->asset_amount * percentage / 100);
+        should_release_amount = should_release_amount - lr->released_amount;
+        if (should_release_amount <= 0) {
+            print("release amount reach max");
+            return;
+        }
+
+        asset a{should_release_amount, 0};
+        subbalance(who, a);
+        withdraw_asset(_self, who, a.asset_id, a.amount);
+
+        lockrules.modify(lr, 0, [&](auto &l) {
+            l.released_amount += should_release_amount;
+        });
+
+        if (lr->released_amount == lr->asset_amount && who_it->assets[0].amount == 0) {
+            print("release finished");
+            lockrules.erase(lr);
+        }
     }
 
   private:
@@ -147,91 +202,9 @@ class skeleton
         return 0;
     }
 
-    //////////////////////////////////////////////////////
-
-    //@abi table lockrule i64
-    struct lockrule {
-        uint64_t account_name;            //某个合约参与者的账号
-        uint64_t lock_time_point;         //锁定开始时间
-        uint64_t lock_time;               //锁定多久开始释放
-        uint64_t lock_release_time_point; //释放开始时间
-        uint64_t lock_release_time;       //释放多久全部释放完毕
-        uint64_t asset_it;                //资产id
-
-        uint64_t primary_key() const { return account_name; }
-
-        GXBLIB_SERIALIZE(lockrule, (account_name)(lock_time_point)(lock_time)(lock_release_time_point)(lock_release_time)(asset_it))
-    };
-
-    typedef graphene::multi_index<N(lockrule), lockrule> lock_rule_index;
-
-  public:
-    /// @abi action
-    void lockasset(uint64_t from, account_name to, asset value)
-    {
-        auto lr = lockrules.find(from);
-        gxb_assert(lr == lockrules.end(), "have been locked, can only lock one time");
-
-        lockrules.emplace(from, [&](auto &a) {
-            a.account_name = to;
-            a.lock_time_point = get_head_block_time();
-            a.lock_time = 30;
-            a.lock_release_time_point = a.lock_time_point + a.lock_time;
-            a.lock_release_time = 3 * 100;
-            a.asset_it = value.asset_id;
-        });
-
-        transfer_asset(from, _self, value.asset_id, value.amount);
-        addbalance(to, value);
-    }
-
-    /// @abi action
-    void tryrelease(uint64_t who)
-    {
-        if (canrelease(who)) {
-            release(who);
-        }
-    }
-
   private:
-    bool canrelease(uint64_t who)
-    {
-        auto lr = lockrules.find(who);
-        gxb_assert(lr != lockrules.end(), "have no locked asset");
-
-        uint64_t cur_time = get_head_block_time();
-        if (cur_time > lr->lock_release_time_point) {
-            return true;
-        }
-
-        return false;
-    }
-
-    int64_t getreleaseamount(uint64_t who)
-    {
-        auto lr = lockrules.find(who);
-        auto who_it = accounts.find(who);
-        gxb_assert(lr != lockrules.end(), "have no locked asset");
-        gxb_assert(who_it != accounts.end(), "have no locked asset");
-
-        uint64_t cur_time = get_head_block_time();
-        int percentage = (cur_time - lr->lock_release_time_point) * 100 / lr->lock_release_time;
-        print("percentage = ${p}", ("p", percentage));
-
-        return (int64_t)(1.0f * who_it->assets[0].amount * percentage / 100);
-    }
-
-    void release(uint64_t who)
-    {
-        int64_t amt = getreleaseamount(who);
-        asset a{amt, 0};
-        withdraw(_self, who, a);
-    }
-
-  private:
-    account_name _self;
     account_index accounts;
-    lock_rule_index lockrules;
+    lockrule_index lockrules;
 };
 
-GXB_ABI(skeleton, (deposit)(withdraw)(lockasset)(tryrelease))
+GXB_ABI(skeleton, (lockasset)(tryrelease))
---
+++ contracts/gxblib/contract_old.hpp
@@ -0,0 +1,118 @@
+#pragma once
+#include <gxblib/gxb.hpp>
+#include <gxblib/asset.hpp>
+#include <gxblib/global.h>
+#include <gxblib/system.h>
+#include <gxblib/asset.h>
+#include <gxblib/multi_index.hpp>
+
+namespace graphene {
+
+class contract
+{
+  public:
+    contract(account_name n)
+        : _self(n),
+          accounts(_self, _self)
+    {
+    }
+
+    inline account_name get_self() const { return _self; }
+
+  protected:
+    account_name _self;
+
+  public:
+    //@abi table account i64
+    struct account {
+        account_name owner;
+        asset balance;
+
+        uint64_t primary_key() const { return owner; }
+    };
+
+    typedef graphene::multi_index<N(accounts), account> account_index;
+
+    account_index accounts;
+
+  public:
+    /// @abi action
+    void deposit(account_name from, asset value)
+    {
+        // from must be msg.sender
+        print("deposit trx origin: ", get_trx_origin(), "\n");
+        print("deposit trx from: ",  from, "\n");
+        print("deposit asset_id: ",  value.asset_id, ", amount:", value.amount, "\n");
+        gxb_assert(get_trx_origin() == from, "no deposit permission");
+        // check amount
+        // gxb_assert(get_trx_value() == value.amount);
+
+        deposit_asset(from, _self, value.asset_id, value.amount);
+        addbalance(from, value);
+        print("balance: ", getbalance(from, value.asset_id), "\n");
+    }
+
+    /// @abi action
+    void withdraw(account_name from, account_name to, asset value)
+    {
+        // from must be msg.sender
+        print("withdraw trx origin: ", get_trx_origin(), "\n");
+        print("withdraw trx from: ",  from, "\n");
+        print("withdraw asset_id: ",  value.asset_id, ", amount:", value.amount, "\n");
+        // gxb_assert(get_trx_origin() == from, "no withdraw permission");
+        // check amount
+        // gxb_assert(get_balance(from, value.symbol) >= value.amount);
+
+        subbalance(from, value);
+        withdraw_asset(_self, to, value.asset_id, value.amount);
+    }
+
+  private:
+    void subbalance(account_name owner, asset value)
+    {
+        auto it = accounts.find(owner);
+        if(it == accounts.end()) {
+            print("account not found");
+            return;
+        }
+
+        if (it->balance.amount == value.amount) {
+            accounts.erase(it);
+        } else {
+            accounts.modify(it, owner, [&](auto &a) {
+                a.balance -= value;
+            });
+        }
+    }
+
+    void addbalance(account_name owner, asset value)
+    {
+        auto it = accounts.find(owner);
+        if (it == accounts.end()) {
+            accounts.emplace(owner, [&](auto &a) {
+                print("addbalance, owner: ", owner, ", asset_id: ", value.asset_id, ", amount: ", value.amount, "\n");
+                a.owner = owner;
+                a.balance = value;
+
+            });
+        } else {
+            print("owner exist, to modify");
+            accounts.modify(it, 0, [&](auto &a) {
+                a.balance += value;
+            });
+        }
+    }
+
+    uint64_t getbalance(account_name owner, uint64_t asset_id)
+    {
+        auto it = accounts.find(owner);
+        if(it == accounts.end()) {
+            print("account not found");
+            return 0;
+        }
+
+        return it->balance.amount;
+    }
+};
+
+} /// namespace graphene
