c82d14871857e19bc77b198a112518f6a7465b99
liukunyu <boss.liuky@gmail.com>
2018-07-19 19:17:08 +0800

    update

--- libraries/chain/contract_evaluator.cpp
+++ libraries/chain/contract_evaluator.cpp
@@ -132,7 +132,6 @@ operation_result contract_call_evaluator::do_apply(const contract_call_operation
     dlog("ram_fee=${rf}, cpu_fee=${cf}, ram_usage=${ru}, cpu_usage=${cu}, ram_price=${rp}, cpu_price=${cp}",
             ("rf",ram_fee)("cf",cpu_fee)("ru",ctx.get_ram_usage())("cu",trx_context.get_cpu_usage())("rp",fee_param.price_per_kbyte_ram)("cp",fee_param.price_per_ms_cpu));
     
-//    contract_receipt receipt(contract_receipt::fee_detail(fee.asset_id, ram_fee), contract_receipt::fee_detail(fee.asset_id, cpu_fee));
     contract_receipt receipt{{fee.asset_id, ram_fee}, {fee.asset_id, cpu_fee}};
     return receipt;
 } FC_CAPTURE_AND_RETHROW((op)) }
