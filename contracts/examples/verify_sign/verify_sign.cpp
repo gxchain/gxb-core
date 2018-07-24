256bf0126e1fb25b72a337017fc07d6e48f91390
zhuliting <zhuliting@foxmail.com>
2018-07-24 20:01:36 +0800

    updated verify_sig api and contract example

--- contracts/examples/verify_sign/verify_sign.cpp
+++ contracts/examples/verify_sign/verify_sign.cpp
@@ -16,18 +16,16 @@ class verify_sign : public contract
 
     void verify(std::string &raw_string, signature &sig, std::string &pub_key)
     {
-        char *pub_key_data;
         int pub_key_len = pub_key.length();
-        pub_key_data = (char *)malloc((pub_key_len) * sizeof(char));
-        pub_key.copy(pub_key_data, pub_key_len);
+        char *pub_key_data = (char *)malloc((pub_key_len + 1) * sizeof(char));
+        pub_key.copy(pub_key_data, pub_key_len, 0);
 
-        char *data;
         int len = raw_string.length();
-        data = (char *) malloc((len) * sizeof(char));
+        char *data = (char *) malloc((len + 1) * sizeof(char));
         raw_string.copy(data, len, 0);
 
-        print("pub_key, ", pub_key, "\n");
         print("raw_string, ", raw_string, "\n");
+        print("pub_key, ", pub_key, "\n");
 
         if (verify_signature(data, raw_string.size(), &sig, pub_key_data, pub_key_len)) {
             print("true", "\n");
