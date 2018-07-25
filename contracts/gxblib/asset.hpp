4ada624edcad289e537a464c36a358184db25ee7
zhuliting <zhuliting@foxmail.com>
2018-07-25 17:02:02 +0800

    contract asset_id use low 46bit

--- contracts/gxblib/asset.hpp
+++ contracts/gxblib/asset.hpp
@@ -8,11 +8,12 @@
 namespace graphene {
 
 extern const int64_t scaled_precision_lut[];
+#define GRAPHENE_DB_MAX_INSTANCE_ID  (uint64_t(-1)>>16)
 
 struct asset {
     asset(int64_t a = 0, uint64_t id = 0)
         : amount(a)
-        , asset_id(id)
+        , asset_id(id & GRAPHENE_DB_MAX_INSTANCE_ID)
     {
         gxb_assert(is_amount_within_range(), "magnitude of asset amount must be less than 2^62");
     }
