7aec71733d041988d65b57011d3acf5ee5074cc1
zhuliting <zhuliting@foxmail.com>
2018-08-08 18:06:13 +0800

    update verisy_sign contract

--- contracts/examples/verify_sign/verify_sign.cpp
+++ contracts/examples/verify_sign/verify_sign.cpp
@@ -3,6 +3,7 @@
 #include <graphenelib/dispatcher.hpp>
 #include <graphenelib/print.hpp>
 #include <graphenelib/types.h>
+#include <graphenelib/global.h>
 
 using namespace graphene;
 
@@ -18,6 +19,9 @@ class verify_sign : public contract
     {
         int ret = verify_signature(raw_string.c_str(), raw_string.length(), &sig, pub_key.c_str(), pub_key.length());
         print("ret code, ", ret, "\n");
+
+        std::string symbol = "GXS";
+        print(symbol, "id, ", get_asset_id(symbol.c_str(), symbol.size()));
     }
 };
 
