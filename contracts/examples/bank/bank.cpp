a94fa6b8b71cb0c9464b34642b75daae97bf18b0
liukunyu <boss.liuky@gmail.com>
2018-08-16 13:43:01 +0800

    style: 1.delete print clause. 2.use const reference for lambda parameters

--- contracts/examples/bank/bank.cpp
+++ contracts/examples/bank/bank.cpp
@@ -4,9 +4,7 @@
 #include <graphenelib/dispatcher.hpp>
 #include <graphenelib/global.h>
 #include <graphenelib/multi_index.hpp>
-#include <graphenelib/print.hpp>
 #include <graphenelib/system.h>
-#include <graphenelib/global.h>
 #include <vector>
 
 using namespace graphene;
@@ -31,21 +29,16 @@ class bank : public contract
         uint64_t owner = get_trx_sender();
         auto it = accounts.find(owner);
         if (it == accounts.end()) {
-            print("owner not exist, to add owner");
             accounts.emplace(owner, [&](auto &o) {
                 o.owner = owner;
                 o.balances.emplace_back(amount);
             });
         } else {
-            print("owner exist");
             uint16_t asset_index = std::distance(it->balances.begin(),
-                    find_if(it->balances.begin(), it->balances.end(), [&](contract_asset a) { return a.asset_id == asset_id; })
-                    );
+                                                 find_if(it->balances.begin(), it->balances.end(), [&](const auto &a) { return a.asset_id == asset_id; }));
             if (asset_index < it->balances.size()) {
-                print("asset exist, to update");
                 accounts.modify(it, 0, [&](auto &o) { o.balances[asset_index] += amount; });
             } else {
-                print("asset not exist, to add");
                 accounts.modify(it, 0, [&](auto &o) { o.balances.emplace_back(amount); });
             }
         }
@@ -65,8 +58,6 @@ class bank : public contract
         for (auto asset_it = it->balances.begin(); asset_it != it->balances.end(); ++asset_it) {
             if ((amount.asset_id) == asset_it->asset_id) {
                 graphene_assert(asset_it->amount >= amount.amount, "balance not enough");
-                print("contract total amount: ", asset_it->amount, "\n");
-                print("withdraw amount: ", amount.amount, "\n");
                 if (asset_it->amount == amount.amount) {
                     accounts.modify(it, 0, [&](auto &o) {
                         o.balances.erase(asset_it);
