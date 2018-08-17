9bbbe438d25318bed8ab505bf7cee5f25a72abf5
zhuliting <zhuliting@foxmail.com>
2018-08-17 14:50:37 +0800

    style: remove blank line

--- contracts/examples/linear_vesting_asset/linear_vesting_asset.cpp
+++ contracts/examples/linear_vesting_asset/linear_vesting_asset.cpp
@@ -22,20 +22,20 @@ class linear_vesting_asset : public contract
     void vestingcreate(std::string to, int64_t lock_duration, int64_t release_duration)
     {
         contract_asset ast{get_action_asset_amount(), get_action_asset_id()};
-        
+
         int64_t to_account_id = get_account_id(to.c_str(), to.size());
         graphene_assert(to_account_id >= 0, "invalid account_name to");
-        
+
         auto lr = vestingrules.find(to_account_id);
         graphene_assert(lr == vestingrules.end(), "have been locked, can only lock one time");
 
         vestingrules.emplace(0, [&](auto &o) {
             o.account_id = to_account_id;
-            
+
             o.asset_id = ast.asset_id;
             o.vesting_amount = ast.amount;
             o.vested_amount = 0;
-            
+
             o.lock_time_point = get_head_block_time();
             o.lock_duration = lock_duration;
             o.release_time_point = o.lock_time_point + o.lock_duration;
@@ -48,7 +48,7 @@ class linear_vesting_asset : public contract
     {
         int64_t who_account_id = get_account_id(who.c_str(), who.size());
         graphene_assert(who_account_id >= 0, "invalid account_name to");
-        
+
         auto lr = vestingrules.find(who_account_id);
         graphene_assert(lr != vestingrules.end(), "current account have no locked asset");
 
@@ -78,11 +78,11 @@ class linear_vesting_asset : public contract
     //@abi table vestingrule i64
     struct vestingrule {
         uint64_t account_id;
-        
+
         uint64_t asset_id;          //资产id
         int64_t vesting_amount;     //初始锁定资产
         int64_t vested_amount;      //已释放资产
-        
+
         int64_t lock_time_point;    //锁定开始时间
         int64_t lock_duration;      //锁定多久开始释放
         int64_t release_time_point; //释放开始时间
