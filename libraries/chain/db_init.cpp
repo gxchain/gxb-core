d4238926ab8ceb43cdad29c4d6296e4776f0dcaf
zhuliting <zhuliting@foxmail.com>
2018-08-17 14:26:14 +0800

    style: remove unused comment

--- libraries/chain/db_init.cpp
+++ libraries/chain/db_init.cpp
@@ -182,18 +182,6 @@ const uint8_t key_value_object::type_id;
 // const uint8_t template index64_object::space_id;
 // const uint8_t template index64_object::type_id;
 
-// const uint8_t index128_object::space_id;
-// const uint8_t index128_object::type_id;
-//
-// const uint8_t index256_object::space_id;
-// const uint8_t index256_object::type_id;
-//
-// const uint8_t index_double_object::space_id;
-// const uint8_t index_double_object::type_id;
-//
-// const uint8_t index_long_double_object::space_id;
-// const uint8_t index_long_double_object::type_id;
-
 void database::initialize_evaluators()
 {
    _operation_evaluators.resize(255);
@@ -335,10 +323,6 @@ void database::initialize_indexes()
    add_index< primary_index< table_id_multi_index> >();
    add_index< primary_index< key_value_index> >();
    add_index< primary_index< index64_index> >();
-   // add_index< primary_index< index128_index> >();
-   // add_index< primary_index< index256_index> >();
-   // add_index< primary_index< index_double_index> >();
-   // add_index< primary_index< index_long_double_index> >();
 
 }
 
