112e9c633762edbfe04a171c85d0162325b7e12f
zhuliting <zhuliting@foxmail.com>
2018-08-31 18:08:30 +0800

    feat: struct full_account remove limit_orders

--- libraries/app/include/graphene/app/full_account.hpp
+++ libraries/app/include/graphene/app/full_account.hpp
@@ -25,7 +25,6 @@
 
 #include <graphene/chain/account_object.hpp>
 #include <graphene/chain/vesting_balance_object.hpp>
-#include <graphene/chain/market_evaluator.hpp>
 #include <graphene/chain/withdraw_permission_object.hpp>
 
 namespace graphene { namespace app {
@@ -43,8 +42,6 @@ namespace graphene { namespace app {
       vector<account_balance_object>   balances;
       vector<lock_balance_object>      locked_balances;
       vector<vesting_balance_object>   vesting_balances;
-      vector<limit_order_object>       limit_orders;
-      vector<call_order_object>        call_orders;
       vector<proposal_object>          proposals;
       vector<asset_id_type>            assets;
       vector<withdraw_permission_object> withdraws;
@@ -63,8 +60,6 @@ FC_REFLECT( graphene::app::full_account,
             (balances)
             (locked_balances)
             (vesting_balances)
-            (limit_orders)
-            (call_orders)
             (proposals) 
             (assets)
             (withdraws)
