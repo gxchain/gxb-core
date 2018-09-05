ebae7796b3f5f8a323592ad56f29e0ce8379a074
zhuliting <zhuliting@foxmail.com>
2018-09-05 15:31:15 +0800

    test: for local test

--- libraries/chain/include/graphene/chain/hardfork.hpp
+++ libraries/chain/include/graphene/chain/hardfork.hpp
@@ -52,7 +52,8 @@
 // core asset hardfork
 // 2018-11-01 00:00:00
 #ifndef HARDFORK_1008_TIME
-#define HARDFORK_1008_TIME (fc::time_point_sec( 1541001600 ))
+// #define HARDFORK_1008_TIME (fc::time_point_sec( 1541001600 ))
+#define HARDFORK_1008_TIME (fc::time_point_sec( 1536132600 )) // for test
 #endif
 
 // #357 Disallow publishing certain malformed price feeds
