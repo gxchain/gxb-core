7200a9e22a5067d5e965c32b4432c12b81d4a19b
zhuliting <zhuliting@foxmail.com>
2018-09-11 11:43:36 +0800

    feat: devnet 1008 hf time UTC "2018-09-11 08:00:00"

--- libraries/chain/include/graphene/chain/hardfork.hpp
+++ libraries/chain/include/graphene/chain/hardfork.hpp
@@ -50,9 +50,9 @@
 
 // hardfork 1008
 // core asset hardfork
-// "2018-09-11 04:00:00"
+// UTC "2018-09-11 08:00:00"
 #ifndef HARDFORK_1008_TIME
-#define HARDFORK_1008_TIME (fc::time_point_sec( 1536609600 )) // for devnet
+#define HARDFORK_1008_TIME (fc::time_point_sec( 1536624000 )) // for devnet
 #endif
 
 // #357 Disallow publishing certain malformed price feeds
