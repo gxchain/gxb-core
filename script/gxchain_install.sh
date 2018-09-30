ee969d65d1833188124df3e9814ee231bf37ed0e
zhuliting <zhuliting@foxmail.com>
2018-09-30 17:23:33 +0800

    feat: add install script

---
+++ script/gxchain_install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget 'http://gxb-package.oss-cn-hangzhou.aliyuncs.com/gxb-core/gxb_ubuntu_1.0.180929.tar.gz' -O gxb_ubuntu_1.0.180929.tar.gz
+tar zxvf gxb_ubuntu_1.0.180929.tar.gz
---
+++ script/gxchain_testnet_install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget 'http://gxb-package.oss-cn-hangzhou.aliyuncs.com/gxb-core/testnet/gxb_ubuntu_1.0.180926.testnet.tar.gz' -O gxb_ubuntu_1.0.180926.testnet.tar.gz
+tar zxvf gxb_ubuntu_1.0.180926.testnet.tar.gz
