f1ad14ffb703e9ffd8086e51042f0abff7b88bc1
zhuliting <zhuliting@foxmail.com>
2018-10-09 15:41:28 +0800

    docs: updated package download url

--- script/gxchain_install.sh
+++ script/gxchain_install.sh
@@ -6,7 +6,7 @@ if [ "$ARCH" == "Linux" ]; then
         printf "\\n\\tGXChain core currently supports macOS & Ubuntu Linux only.\\n"
         exit 1
     fi
-    curl -L 'http://gxb-package.oss-cn-hangzhou.aliyuncs.com/gxb-core/gxb_ubuntu_1.0.180929.tar.gz' -o gxb_ubuntu_1.0.180929.tar.gz
+    curl -L 'https://github.com/gxchain/gxb-core/releases/download/v1.0.180929/gxb_ubuntu_1.0.180929.tar.gz' -o gxb_ubuntu_1.0.180929.tar.gz
     tar zxvf gxb_ubuntu_1.0.180929.tar.gz
 elif [ "$ARCH" == "Darwin" ]; then
     curl -L 'https://github.com/gxchain/gxb-core/releases/download/v1.0.180929/gxb_osx_1.0.180929.tar.gz' -o gxb_osx_1.0.180929.tar.gz
