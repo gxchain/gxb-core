e293d5c328accbf6527323dd8f2830d0acad7a2e
zhuliting <zhuliting@foxmail.com>
2018-10-26 14:03:07 +0800

    chore: updated got block log, add trx size

--- libraries/app/application.cpp
+++ libraries/app/application.cpp
@@ -488,9 +488,10 @@ namespace detail {
             const auto& witness = blk_msg.block.witness(*_chain_db);
             const auto& witness_account = witness.witness_account(*_chain_db);
             auto last_irr = _chain_db->get_dynamic_global_properties().last_irreversible_block_num;
-            ilog("Got block: #${n} time: ${t} latency: ${l} ms from: ${w}  irreversible: ${i} (-${d})",
+            ilog("Got block: #${n} time: ${t} transaction(s): ${x} latency: ${l} ms from: ${w}  irreversible: ${i} (-${d})",
                  ("t",blk_msg.block.timestamp)
                  ("n", blk_msg.block.block_num())
+                 ("x", blk_msg.block.transactions.size())
                  ("l", (latency.count()/1000))
                  ("w",witness_account.name)
                  ("i",last_irr)("d",blk_msg.block.block_num()-last_irr) );
