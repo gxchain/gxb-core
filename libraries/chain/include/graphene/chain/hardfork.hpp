abd0b396e2ab76595ed95356c93f630e36f710e8
liukunyu <boss.liuky@gmail.com>
2018-11-12 11:00:01 +0800

    remove hardfork_1103_time

--- libraries/chain/include/graphene/chain/hardfork.hpp
+++ libraries/chain/include/graphene/chain/hardfork.hpp
@@ -121,9 +121,5 @@
 // #615 Fix price feed expiration check, so websocket server will never spam too much data
 #ifndef HARDFORK_615_TIME
 #define HARDFORK_615_TIME (fc::time_point_sec( 1458752400 ))
-// #1103 the time point before you can renaming asset symbol
-#ifndef HARDFORK_1103_TIME
-#define HARDFORK_1103_TIME (fc::time_point_sec( 1541203200 ))
-#endif
 
 #endif
