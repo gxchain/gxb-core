788b720452cf978a36a349f5e7ea0f921b8b6b81
zhuliting <zhuliting@foxmail.com>
2018-11-15 14:00:12 +0800

    feat: add get_account_name_by_id api

--- libraries/chain/wasm_interface.cpp
+++ libraries/chain/wasm_interface.cpp
@@ -202,6 +202,20 @@ class global_api : public context_aware_api
         return -1;
     }
 
+    int64_t get_account_name_by_id(array_ptr<char> data, int64_t account_id)
+    {
+        FC_ASSERT(accout_id >= 0, "account_id ${a} must > 0", ("a", account_id));
+        auto &d = context.db();
+        auto obj = d.find(account_id_type(account_id));
+        if (obj) {
+            string account_name = o->name;
+            memcpy(data, account_name.c_str(), account_name.size());
+            return 0;
+        }
+        // account not exist, return -1
+        return -1;
+    }
+
     int64_t get_asset_id(array_ptr<char> data, size_t datalen)
     {
         std::string symbol(data, datalen);
