f3423c5640e13a477a9199a96b76226a224412ba
zhuliting <zhuliting@foxmail.com>
2018-11-26 19:22:55 +0800

    chore: updated testnet install script

--- script/gxchain_testnet_install.sh
+++ script/gxchain_testnet_install.sh
@@ -6,11 +6,11 @@ if [ "$ARCH" == "Linux" ]; then
         printf "\\n\\tGXChain core currently supports macOS & Ubuntu Linux only.\\n"
         exit 1
     fi
-    curl -L 'http://gxb-package.oss-cn-hangzhou.aliyuncs.com/gxb-core/testnet/gxb_ubuntu_1.0.1801029.testnet.tar.gz' -o gxb_ubuntu_1.0.1801029.testnet.tar.gz
-    tar zxvf gxb_ubuntu_1.0.1801029.testnet.tar.gz
+    curl -L 'https://github.com/gxchain/gxb-core/releases/download/testnet-1.0.181126/gxb_ubuntu_1.0.181126.testnet.tar.gz' -o gxb_ubuntu_1.0.181126.testnet.tar.gz
+    tar zxvf gxb_ubuntu_1.0.181126.testnet.tar.gz
 elif [ "$ARCH" == "Darwin" ]; then
-    curl -L 'http://gxb-package.oss-cn-hangzhou.aliyuncs.com/gxb-core/testnet/gxb_testnet_osx_1.0.180929.tar.gz' -o gxb_testnet_osx_1.0.180929.tar.gz
-    tar zxvf gxb_testnet_osx_1.0.180929.tar.gz
+    curl -L 'https://github.com/gxchain/gxb-core/releases/download/testnet-1.0.181126/gxb_osx_1.0.181126.testnet.tar.gz' -o gxb_osx_1.0.181126.testnet.tar.gz
+    tar zxvf gxb_osx_1.0.181126.testnet.tar.gz
 else
     printf "\\n\\tGXChain core currently supports macOS & Ubuntu Linux only.\\n"
     exit 1
