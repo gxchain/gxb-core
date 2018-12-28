00ca8db13603e476bf31fd448b43f130c7a61658
zhuliting <zhuliting@foxmail.com>
2018-12-28 15:44:58 +0800

    add read_transaction for private-net

--- libraries/chain/wasm_interface.cpp
+++ libraries/chain/wasm_interface.cpp
@@ -1080,7 +1080,6 @@ class context_free_transaction_api : public context_aware_api {
       context_free_transaction_api( apply_context& ctx )
       :context_aware_api(ctx,true){}
 
-      /*
       int read_transaction(array_ptr<char> data, size_t buffer_size)
       {
           const transaction* cur_trx = context.db().get_cur_trx();
@@ -1101,7 +1100,6 @@ class context_free_transaction_api : public context_aware_api {
           FC_ASSERT(nullptr != trx, "current transaction not set");
           return fc::raw::pack(*trx).size();
       }
-      */
 
       uint64_t expiration() {
           return context.db().get_cur_trx()->expiration.sec_since_epoch();
@@ -1568,10 +1566,8 @@ REGISTER_INTRINSICS(transaction_api,
 );
 
 REGISTER_INTRINSICS(context_free_transaction_api,
-/*
 (read_transaction,               int(int, int))
 (transaction_size,               int())
-*/
 (expiration,                     int64_t())
 (tapos_block_num,                int())
 (tapos_block_prefix,             int64_t())
