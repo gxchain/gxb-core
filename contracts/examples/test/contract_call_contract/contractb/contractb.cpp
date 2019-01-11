8261de05048357afbf7cb6501213de0864da5b9f
liukunyu <boss.liuky@gmail.com>
2019-01-11 18:29:01 +0800

    feat: 1.from hardfork 1015 on, the ram payer must be 0 or sender.\n 2.add statictics for ram usage by payer which can be used for ram fee charging

--- contracts/examples/test/contract_call_contract/contractb/contractb.cpp
+++ contracts/examples/test/contract_call_contract/contractb/contractb.cpp
@@ -3,6 +3,9 @@
 #include <graphenelib/contract.hpp>
 #include <graphenelib/dispatcher.hpp>
 #include <graphenelib/print.hpp>
+#include <graphenelib/global.h>
+#include <graphenelib/multi_index.hpp>
+#include <../parameters.hpp>
 
 using namespace graphene;
 
@@ -14,19 +17,24 @@ class contractb : public contract
     {
     }
 
-    struct p {
-    	bool circlecall;
-    };
-
     // @abi action
     // @abi payable
-    void hi(uint64_t a, uint64_t b)
+    void hi(uint64_t ca_id, uint64_t cb_id, uint64_t cc_id, uint64_t pk, uint64_t payer)
     {
-        uint64_t c = a+b;
-        print("hi im contractb, the sum(a,b)=", c, "\n");
-        p p1{true};
-        action bb(528, N(hi), std::move(p1), _self, {100000, 1});//call contractc(function hi)
-        bb.send();
+        int64_t asset_amount = get_action_asset_amount();
+        uint64_t asset_id = get_action_asset_id();
+        uint64_t sender = get_trx_sender();
+        uint64_t origin = get_trx_origin();
+        uint64_t me = current_receiver();
+        print("asset_amount=", asset_amount, "\n");
+        print("asset_id=", asset_id, "\n");
+        print("sender=", sender, "\n");
+        print("origin=", origin, "\n");
+        print("current_contract=", me, "\n");
+
+        p p1{ca_id, cb_id, cc_id, pk, payer};
+        action b(cc_id, N(hi), std::move(p1), _self, {100000, 1});
+        b.send();
     }
 };
 
---
+++ contracts/examples/test/contract_call_contract/parameters.hpp
@@ -0,0 +1,9 @@
+#include <stdint.h>
+
+struct p {
+	uint64_t ca_id;
+	uint64_t cb_id;
+	uint64_t cc_id;
+	uint64_t pk;
+	uint64_t payer;
+};
