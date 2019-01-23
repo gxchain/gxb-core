1528599db6b400bb0773ed994644fc60505e2330
zhuliting <zhuliting@foxmail.com>
2019-01-23 15:32:40 +0800

    removed duplicate code

--- libraries/chain/db_init.cpp
+++ libraries/chain/db_init.cpp
@@ -213,7 +213,6 @@ void database::initialize_evaluators()
    register_evaluator<call_order_update_evaluator>();
    register_evaluator<transfer_evaluator>();
    register_evaluator<inline_transfer_evaluator>();
-   register_evaluator<inline_transfer_evaluator>();
    register_evaluator<override_transfer_evaluator>();
    register_evaluator<asset_fund_fee_pool_evaluator>();
    register_evaluator<asset_publish_feeds_evaluator>();
