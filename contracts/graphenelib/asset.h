74f324035fb44d87e671ffa0f9689311e4d91bd9
zhuliting <zhuliting@foxmail.com>
2019-01-24 16:18:00 +0800

    declare inline_transfer api

--- contracts/graphenelib/asset.h
+++ contracts/graphenelib/asset.h
@@ -5,5 +5,6 @@ extern "C" {
 
     void withdraw_asset(uint64_t from, uint64_t to, uint64_t asset_id, int64_t amount);
     int64_t get_balance(int64_t account, int64_t asset_id);
+    void inline_transfer(uint64_t from, uint64_t to, uint64_t asset_id, int64_t amount, const char* data, uint32_t length);
 
 }
