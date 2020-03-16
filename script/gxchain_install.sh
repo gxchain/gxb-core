4afe77d0b140b154a3d3fe97450e3a9e8cb1db12
beejianing <ddpfvt@163.com>
2020-03-16 16:33:46 +0800

    shore::update the gxchain_install.sh

--- script/gxchain_install.sh
+++ script/gxchain_install.sh
@@ -11,11 +11,11 @@ if [ "$ARCH" == "Linux" ]; then
     OS_VERSION=$(lsb_release -a | grep "Release" | sed -e 's/Release:[\t]*//g')
     echo "Your OS Version: Ubuntu ${OS_VERSION}"
     if [ "$OS_VERSION" == "14.04" ]; then
-        curl -L 'https://github.com/gxchain/gxb-core/releases/download/v1.0.190318/gxb_1.0.190318-ubuntu-14.04.tar.gz' -o gxb_1.0.190318-ubuntu-14.04.tar.gz
-        tar zxvf gxb_1.0.190318-ubuntu-14.04.tar.gz
+        curl -L 'https://github.com/gxchain/gxb-core/releases/download/v1.0.200327/gxb_1.0.200327-ubuntu-14.04.tar.gz' -o gxb_1.0.200327-ubuntu-14.04.tar.gz 
+        tar zxvf gxb_1.0.200327-ubuntu-14.04.tar.gz
     else
-        curl -L 'https://github.com/gxchain/gxb-core/releases/download/v1.0.190318/gxb_1.0.190318-ubuntu-14.04.tar.gz' -o gxb_1.0.190318-ubuntu-14.04.tar.gz
-        tar zxvf gxb_1.0.190318-ubuntu-14.04.tar.gz
+        curl -L 'https://github.com/gxchain/gxb-core/releases/download/v1.0.200327/gxb_1.0.200327-ubuntu-14.04.tar.gz' -o gxb_1.0.200327-ubuntu-14.04.tar.gz 
+        tar zxvf gxb_1.0.200327-ubuntu-14.04.tar.gz
     fi
 elif [ "$ARCH" == "Darwin" ]; then
     curl -L 'https://github.com/gxchain/gxb-core/releases/download/v1.0.190318/gxb_1.0.190318-osx.tar.gz' -o gxb_1.0.190318-osx.tar.gz
