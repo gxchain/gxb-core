f334c16c58733e9d1c3f87f2ea97a7812bbbfbbc
sspin <>
2020-11-26 13:07:57 +0800

    chore : add the nft contract

---
+++ contracts/examples/gxc-nft/README.md
@@ -0,0 +1,112 @@
+## NFT-Contract
+
+### 静态变量
+```cpp
+static const int64_t BLACKHOLEACCOUNT   = 3;    // 黑洞账户.
+```
+
+
+### 数据结构
+#### token表
+- 用于记录每一个独特的token的信息
+```cpp
+struct token{
+            uint64_t tokenid;//主键，token的独特id，不能一样
+            std::string tokenuri;//对与token的描述
+            uint64_t owner;//所有者
+            uint64_t approve;//授权操作者
+            uint64_t primary_key() const { return tokenid; }
+            GRAPHENE_SERIALIZE(token,(tokenid)(tokenuri)(owner)(approve))
+        };
+        typedef graphene::multi_index<N(token),token> token_index;
+```
+
+#### account表
+- 用于记录每个用户所拥有的token信息
+```cpp
+struct account
+        {
+           uint64_t owner;//主键，用户的id
+           std::set<uint64_t> tokenids;//set，记录用户所有的tokenid
+           std::set<uint64_t> allowance;//记录用户完全授权的信息
+           uint64_t primary_key() const { return owner; }
+           GRAPHENE_SERIALIZE(account,(owner)(tokenids)(allowance))
+        };
+        typedef graphene::multi_index<N(account),account> account_index;
+```
+
+### 接口
+#### 1. 查询代币持有者
+- 根据_tokenId 返回代币持有者id
+```cpp
+//@abi action
+uint64_t ownerof(uint64_t tokenid){
+        //检查现token是否存在
+        //查找到token的owner并返回
+}
+```
+#### 2.mint创建token
+- mint创建一个token,并将其分发给用户
+```cpp
+//@abi action
+void mint(uint64_t to, uint64_t tokenid, std::string tokenuri){
+        //检查token是否已经存在
+        //创建token，将其信息入tokens表
+        //将token信息加入用户的收藏表中
+
+}
+```
+#### 3.授权token的交易权
+- approve会授权其他的用户token的交易权
+```cpp
+//@abi action
+void approve(uint64_t tokenid, uint64_t to){
+        //检查token是否存在
+        //检查发送者是否被完全授权或者是否为拥有者
+        //更改该token的交易权授权人，默认状态下为3，黑洞账户
+}
+```
+#### 4.将一个用户所有权限授予另一个用户
+- approveall允许用户将名下所有收藏的权限赋予另一个用户
+```cpp
+//@abi action
+void approveall(uint64_t to){
+        //检查账户是否存在
+        //给用户授权
+}
+```
+#### 5.移除所有权限
+- appallremove可以让用户移除对另个用户的所有权限的授权
+```cpp
+//@abi action
+void appallremove(uint64_t to){
+        //检查是否有权限
+        //移除权限
+}
+```
+  
+#### 6.交易代币
+- transfer可以让拥有权限的用户交易一个代币
+```cpp
+//@abi action
+void transfer(uint64_t tokenid, uint64_t to){
+        //检查token是否存在
+        //检查发送者是否被完全授权或者是否为拥有者
+        //移除前拥有者的授权权限，将其改为默认的3
+        //将代币拥有者更改
+        //加入现拥有者的收藏
+}       
+```
+#### 7.销毁代币
+- burn销毁代币，代币存在，但无法进行任何的操作
+```cpp
+//@abi action
+void burn(uint64_t tokenid){
+        //检查代币是否存在
+        //检查是否权限修改
+        //将代币拥有者改为3黑洞账户
+        //将代币权限移除为3黑洞账户
+}
+
+```
+
---
+++ contracts/examples/gxc-nft/nft.cpp
@@ -0,0 +1,135 @@
+#include <graphenelib/asset.h>
+#include <graphenelib/contract.hpp>
+#include <graphenelib/contract_asset.hpp>
+#include <graphenelib/global.h>
+#include <graphenelib/multi_index.hpp>
+#include <graphenelib/system.h>
+#include <graphenelib/dispatcher.hpp>
+#include <map>
+#include <set>
+#include <tuple>
+#include <math.h>
+
+using namespace graphene;
+
+static const int64_t BLACKHOLEACCOUNT = 3;
+
+class nft : public contract{
+    public:
+        nft(uint64_t account_id)
+           : contract(account_id)
+           , tokens(_self, _self)
+           , accounts(_self, _self)
+        {
+        }
+
+        //@abi action
+        uint64_t ownerof(uint64_t tokenid){
+            graphene_assert(_exists(tokenid) == true , "The tokenid is not existed");
+            auto token_itr = tokens.find(tokenid);
+            uint64_t owner = token_itr->owner;
+            return owner;
+        }
+
+        //@abi action
+        void mint(uint64_t to, uint64_t tokenid, std::string tokenuri){
+            auto sender = get_trx_sender();
+            graphene_assert(_exists(tokenid) == false , "The tokenid is existed");
+            tokens.emplace(sender,[&](auto &t){
+                t.tokenid = tokenid;
+                t.tokenuri = tokenuri;
+                t.owner = to;
+                t.approve = BLACKHOLEACCOUNT;
+            });
+            auto account_itr = accounts.find(to);
+            if(account_itr == accounts.end()){
+                accounts.emplace(sender,[&](auto &a){
+                    a.owner = to;
+                    a.tokenids.insert(tokenid);
+                });
+            } else {
+                accounts.modify(*account_itr, sender, [&](auto &a){
+                    a.tokenids.insert(tokenid);
+                });
+            }
+        }
+
+        //@abi action
+        void approve(uint64_t tokenid, uint64_t to){
+            auto sender = get_trx_sender();
+            graphene_assert(_exists(tokenid) == true , "The tokenid is not existed");
+            auto token_itr = tokens.find(tokenid);
+            auto owner = token_itr->owner;
+            graphene_assert(owner == sender || _isallapprove(owner, sender) , "You do not have permission");
+            tokens.modify(*token_itr,sender,[&](auto &t){
+                t.approve = to;
+            });
+        }
+
+        //@abi action
+        void transfer(uint64_t tokenid, uint64_t to){
+            auto sender = get_trx_sender();
+            graphene_assert(_exists(tokenid) == true , "The tokenid is not existed");
+        }
+
+        //@abi action
+        void burn(uint64_t tokenid){
+
+        }
+
+        //@abi action 
+        void approveall(uint64_t to){
+    
+        }
+
+        //@abi action
+        void appallremove(uint64_t to){
+            
+        }
+
+    private:
+        uint64_t _getapproved(uint64_t tokenid){
+            graphene_assert(_exists(tokenid) == true , "The tokenid is not existed");
+            auto token_itr = tokens.find(tokenid);
+            uint64_t approveid = token_itr->approve;
+            return approveid;
+        }
+
+        bool _isallapprove(uint64_t owner, uint64_t operator){
+            auto account_itr = accounts.find(owner);
+            graphene_assert( account_itr != accounts.end() , "The account does not have token");
+            auto approve_itr = account_itr->allowance.find(operator);
+            return (approve_itr == account_itr->allowance.end() ? false : true);
+        }
+
+        bool _exists(uint64_t tokenid){
+            auto token_itr = tokens.find(tokenid);
+            return (token_itr == tokens.end() ? false : true);
+        }
+
+        //@abi table token i64
+        struct token
+        {
+            uint64_t tokenid;
+            std::string tokenuri;
+            uint64_t owner;
+            uint64_t approve;
+            uint64_t primary_key() const { return tokenid; }
+            GRAPHENE_SERIALIZE(token,(tokenid)(tokenuri)(owner)(approve))
+        };
+        typedef graphene::multi_index<N(token),token> token_index;
+        token_index tokens;
+
+        //@abi table account i64
+        struct account
+        {
+           uint64_t owner;
+           std::set<uint64_t> tokenids;
+           std::map<uint64_t, bool> allowance;
+           uint64_t primary_key() const { return owner; }
+           GRAPHENE_SERIALIZE(account,(owner)(tokenids)(allowance))
+        };
+        typedef graphene::multi_index<N(account),account> account_index;
+        account_index accounts;
+};
+GRAPHENE_ABI(nft, (mint))
