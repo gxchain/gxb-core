5b6b70cf969ce378e9347303719768c88d2fcb4c
sspin <>
2020-12-17 07:35:17 +0800

    chore: update comment

--- libraries/chain/include/graphene/chain/hardfork.hpp
+++ libraries/chain/include/graphene/chain/hardfork.hpp
@@ -148,7 +148,7 @@
 
 #ifndef HARDFORK_1029_TIME
 
-#define HARDFORK_1029_TIME (fc::time_point_sec( 1608133197 )) // for mainnet, 2020-03-27T00:00:00(UTC)
+#define HARDFORK_1029_TIME (fc::time_point_sec( 1608133197 )) // for mainnet, 2020-12-16T15:39:57(UTC)
 #endif
 
 // #413 Add operation to claim asset fees
